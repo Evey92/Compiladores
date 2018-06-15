c7c184cd20411e615a44c53d26209cf854d91b4b
Evey92 <evey9207@gmail.com>
2018-06-15 01:20:05 -0500

    kbkh

---
+++ CompilerUI/CompilerCore/CSyntaxModule.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "LexicoModule.h"
+#include "CErrModule.h"
+#include "CSyntaxModule.h"
+namespace CompilerCore
+{
+	public class CSyntaxModule
+	{
+	public:
+		CSyntaxModule(LexicoModule lexModule);
+		~CSyntaxModule();
+	};
+
+	CSyntaxModule::CSyntaxModule(LexicoModule lexModule)
+	{
+
+	}
+
+}
+
+
+
